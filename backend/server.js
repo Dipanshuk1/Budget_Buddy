b5bbdae1d26eb5138e9808236bb2639c13ffe809
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-09-27 13:47:03 +0530

    Updated server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-Highlight;
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import connectDB from "./config/db.js";
+
+// Load env variables
+dotenv.config();
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
